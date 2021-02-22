8e8299f087ca7f9c7be41f41ead6482d8ec30d84
Nick Hertzog <nickh671671@gmail.com>
2021-02-21 20:58:27 -0500

    week 7

---
+++ Week7/Hertzog_7_7/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Hertzog_7_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.leastTB = new System.Windows.Forms.TextBox();
+            this.greatestTB = new System.Windows.Forms.TextBox();
+            this.avgTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Average change in population";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Year of greatest increase";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year of least increase";
+            // 
+            // leastTB
+            // 
+            this.leastTB.Location = new System.Drawing.Point(210, 72);
+            this.leastTB.Name = "leastTB";
+            this.leastTB.ReadOnly = true;
+            this.leastTB.Size = new System.Drawing.Size(100, 20);
+            this.leastTB.TabIndex = 3;
+            // 
+            // greatestTB
+            // 
+            this.greatestTB.Location = new System.Drawing.Point(210, 39);
+            this.greatestTB.Name = "greatestTB";
+            this.greatestTB.ReadOnly = true;
+            this.greatestTB.Size = new System.Drawing.Size(100, 20);
+            this.greatestTB.TabIndex = 4;
+            // 
+            // avgTB
+            // 
+            this.avgTB.Location = new System.Drawing.Point(210, 9);
+            this.avgTB.Name = "avgTB";
+            this.avgTB.ReadOnly = true;
+            this.avgTB.Size = new System.Drawing.Size(100, 20);
+            this.avgTB.TabIndex = 5;
+            this.avgTB.TextChanged += new System.EventHandler(this.avgTB_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 124);
+            this.Controls.Add(this.avgTB);
+            this.Controls.Add(this.greatestTB);
+            this.Controls.Add(this.leastTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox leastTB;
+        private System.Windows.Forms.TextBox greatestTB;
+        private System.Windows.Forms.TextBox avgTB;
+    }
+}
+
---
+++ Week7/Hertzog_7_7/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hertzog_7_7
+{
+    public partial class Form1 : Form
+    {
+        //list for the numbers from file and then the changes between years
+        List<int> nums = new List<int>();
+        List<int> change = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            readFile();
+            calculateChanges();
+        }
+
+        private void avgTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //function to calculate the population change between years
+        private void calculateChanges()
+        {
+            //this is something i have been trying to puzzle out
+            //unsure if i should add 0 to account for 1950 or not
+            //because would the value for 1950 - the value for 1951 count as 1951 or 1950?
+            //I have it set up so that value would be for 1951 and 1950 is defaulted to 0
+            change.Add(0);
+
+            //looping through each number in the list
+            //adding the current index - next index to the change list (using absolute value because we dont care about positive of negative, just the change)
+            for(int i = 1; i < (nums.Count - 1); i++)
+            {
+                change.Add(Math.Abs((nums[i] - nums[i + 1])));
+            }
+
+            //three variables used to display data
+            int average = calcAvg(change);
+            int max = calcMost(change);
+            int least = calcLeast(change);
+
+            //data
+            display(average, max, least);
+        }
+
+        //display function to populate the textboxes in the ui
+        private void display(double avg, int most, int least)
+        {
+            avgTB.Text = avg.ToString();
+            greatestTB.Text = most.ToString();
+            leastTB.Text = least.ToString();
+        }
+
+        //function used to calculate the average across a given list
+        //in our case the change list is passed in and the average is returned
+        //using integers because there cant be a fraction of a person
+        private int calcAvg(List<int> nums)
+        {
+            //reasoning for count - 1 is to factor in that 0 we added at the start
+            //we dont want to keep that 0 in this calculation and have it be throwing off our numbers
+            return nums.Sum()/(nums.Count() - 1);
+        } 
+
+        //list is passed in
+        //finds the year with the least amount of change within a given list
+        //returns the index of that value and adds 1950 to it to calculate the year of the change
+        private int calcLeast(List<int> nums)
+        {
+
+            //in this scenario, I am defaulting to the second value at the first index
+            //normally you would want to default to index 0, however, our index 0's value is 0 to account for the year 1950
+            //we want to ignore that year so we will be starting at index 1 here.
+            int least = nums[1];
+            int index = 1;
+
+            //loop through given list
+            for(int i = 1; i < nums.Count; i++)
+            {
+                //if current is less than the least, set least to current
+                if(nums[i] < least)
+                {
+                    least = nums[i];
+                    index = i;
+                }
+            }
+
+            return index + 1950;
+        }
+
+        //list is passed in
+        //finds the year with the most amount of change within a given list
+        //returns the index of that value and adds 1950 to it to calculate the year of the change
+        private int calcMost(List<int> nums)
+        {
+            int max = nums[0];
+            int index = 0;
+            //loop through list
+            for (int i = 1; i < nums.Count; i++)
+            {
+                //if current is more than max, reset max to current
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    index = i;
+                }
+            }
+
+            return index + 1950;
+        }
+
+
+        //function used to read in our txt file
+        //hard coded solution because of file path being stored as string
+        private void readFile()
+        {
+            //opening file with given file path and reading those lines into an array of strings
+            string fpath = "..\\..\\..\\USPopulation.txt";
+            String[] lines = System.IO.File.ReadAllLines(fpath);
+            //converting the string array to an integer array
+            foreach(String line in lines)
+            {
+                nums.Add(Int32.Parse(line));
+            }
+
+        }
+    }
+}
